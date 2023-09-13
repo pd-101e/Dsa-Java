f769ce3c25d0ddd989e90f2d63e6193c6f3917b3
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2023-09-13 16:15:56 +0530

    Update listIterator.java

--- core/listIterator.java
+++ core/listIterator.java
@@ -28,3 +28,34 @@ public class Main {
 
     }
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        List<Object>list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        ListIterator <Object> it = list.listIterator();
+        while(it.hasNext())
+        {
+            it.add(5);
+            it.next();
+        }
+        it = list.listIterator();
+        while (it.hasNext())
+        {
+            int x = (Integer)it.next();
+            System.out.println(x);
+        }
+
+    }
+}
