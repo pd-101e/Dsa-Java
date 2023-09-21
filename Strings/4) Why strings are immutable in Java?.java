997ca8a7b8c0f8d9d713331e6f90b55ab3ff8151
Prathmesh Dakhore <59065854+pd-101e@users.noreply.github.com>
2023-09-21 12:45:37 +0530

    Create 4) Why strings are immutable in Java?.java

---
+++ Strings/4) Why strings are immutable in Java?.java
@@ -0,0 +1,12 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	String str="Knowledge";
+	String s=str;
+	str=str.concat(" Base");
+	System.out.println(str);
+	}
+}
